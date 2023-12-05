f2391ddf843c5fda759659d7f3a61c2f5f1a393a
zarcher01 <zachayarch157@gmail.com>
2023-12-05 15:10:34 -0500

    Updated Populate for Demo

--- api/mysql_scripts/crust_toppings_populate.sql
+++ api/mysql_scripts/crust_toppings_populate.sql
@@ -1,17 +1,44 @@
-use pizza_restaurant_api;
+DROP DATABASE IF EXISTS pizza_restaurant_api2;
+CREATE DATABASE pizza_restaurant_api2;
+USE pizza_restaurant_api2;
 
--- INSERT INTO toppings (name, price) VALUES ('Olives', 1.80);
--- INSERT INTO toppings (name, price) VALUES ('Sausage', 2.25);
--- INSERT INTO toppings (name, price) VALUES ('Bacon', 2.75);
--- INSERT INTO toppings (name, price) VALUES ('Spinach', 1.90);
--- INSERT INTO toppings (name, price) VALUES ('Tomatoes', 1.60);
+#CRUSTS
+INSERT INTO crusts (name, price) 
+VALUES ("Regular", 4.00);
+INSERT INTO crusts (name, price) 
+VALUES ("Pan", 5.00);
+INSERT INTO crusts (name, price) 
+VALUES ("Thin", 4.00);
+INSERT INTO crusts (name, price) 
+VALUES ("Stuffed", 5.00);
+INSERT INTO crusts (name, price) 
+VALUES ("Gluten Free", 5.00);
 
--- select * from toppings;
+#TOPPINGS
+INSERT INTO toppings (name, price)
+VALUES ("Ham", 1.50);
+INSERT INTO toppings (name, price)
+VALUES ("Beef", 2.00);
+INSERT INTO toppings (name, price)
+VALUES ("Salami", 2.50);
+INSERT INTO toppings (name, price)
+VALUES ("Pepperoni", 2.00);
+INSERT INTO toppings (name, price)
+VALUES ("Italian Sausage", 2.00);
+INSERT INTO toppings (name, price)
+VALUES ("Chicken", 2.00);
+INSERT INTO toppings (name, price)
+VALUES ("Bacon", 2.50);
+INSERT INTO toppings (name, price)
+VALUES ("Peppers", 1.00);
+INSERT INTO toppings (name, price)
+VALUES ("Mushrooms", 1.00);
+INSERT INTO toppings (name, price)
+VALUES ("Spinach", 1.00);
+INSERT INTO toppings (name, price)
+VALUES ("Extra Cheese", 2.00);
 
--- INSERT INTO crusts (name, price) VALUES ('Thin Crust', 3.50);
--- INSERT INTO crusts (name, price) VALUES ('Thick Crust', 4.00);
--- INSERT INTO crusts (name, price) VALUES ('Stuffed Crust', 5.00);
--- INSERT INTO crusts (name, price) VALUES ('Gluten-Free Crust', 4.50);
--- INSERT INTO crusts (name, price) VALUES ('Cauliflower Crust', 5.50);
+#PROMOTIONS
+INSERT INTO promotions (name, discount_rate, active, start_date, end_date)
+VALUES ("49er Discount", 0.5, 1, "1968-09-23", "2099-01-01");
 
-select * from crusts;
