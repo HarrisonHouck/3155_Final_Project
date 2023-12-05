4b41bc417b1e385e7c386d410ecacc5da6ab545c
zarcher01 <zachayarch157@gmail.com>
2023-12-05 01:06:17 -0500

    Update Router
    made sure all the functionality of the payment works

--- api/routers/payment_info.py
+++ api/routers/payment_info.py
@@ -19,21 +19,21 @@ def read_all_payment_info(db: Session = Depends(get_db)):
 
 @router.get("/{payment_info_id}", response_model=PaymentInfo)
 def read_one_payment_info(payment_info_id: int, db: Session = Depends(get_db)):
-    payment_info = read_one_payment_info(db, payment_info_id)
+    payment_info = controller.read_one_payment_info(db, payment_info_id)
     if payment_info is None:
         raise HTTPException(status_code=404, detail="PaymentInfo not found")
     return payment_info
 
 @router.put("/{payment_info_id}", response_model=PaymentInfo)
 def update_payment_info(payment_info_id: int, payment_info: PaymentInfoUpdate, db: Session = Depends(get_db)):
-    updated_payment_info = update_payment_info(db, payment_info_id, payment_info)
+    updated_payment_info = controller.update_payment_info(db, payment_info_id, payment_info)
     if updated_payment_info is None:
         raise HTTPException(status_code=404, detail="PaymentInfo not found")
     return updated_payment_info
 
 @router.delete("/{payment_info_id}", response_model=PaymentInfo)
 def delete_payment_info(payment_info_id: int, db: Session = Depends(get_db)):
-    deleted_payment_info = delete_payment_info(db, payment_info_id)
+    deleted_payment_info = controller.delete_payment_info(db, payment_info_id)
     if deleted_payment_info is None:
         raise HTTPException(status_code=404, detail="PaymentInfo not found")
     return deleted_payment_info
