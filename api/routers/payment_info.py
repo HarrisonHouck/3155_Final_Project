4a295162b7456569299423bed01c61b57a117a6e
zarcher01 <zachayarch157@gmail.com>
2023-12-05 00:49:01 -0500

    Update payment_info
    Changed to be more uniform with rest of code

--- api/routers/payment_info.py
+++ api/routers/payment_info.py
@@ -1,24 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from ..controllers import payment_info as controller
 from ..schemas.payment_info import PaymentInfoCreate, PaymentInfo, PaymentInfoUpdate
-from ..controllers.payment_info import (
-    create_payment_info,
-    read_all_payment_info,
-    read_one_payment_info,
-    update_payment_info,
-    delete_payment_info,
-)
 from ..dependencies.database import get_db
 
-router = APIRouter(prefix="/payment_info", tags=["PaymentInfo"])
+router = APIRouter(
+    prefix="/payment_info", 
+    tags=["PaymentInfo"]
+)
 
 @router.post("/", response_model=PaymentInfo)
 def create_payment_info(payment_info: PaymentInfoCreate, db: Session = Depends(get_db)):
-    return create_payment_info(db, payment_info)
+    return controller.create_payment_info(db, payment_info)
 
 @router.get("/", response_model=list[PaymentInfo])
 def read_all_payment_info(db: Session = Depends(get_db)):
-    return read_all_payment_info(db)
+    return controller.read_all_payment_info(db)
 
 @router.get("/{payment_info_id}", response_model=PaymentInfo)
 def read_one_payment_info(payment_info_id: int, db: Session = Depends(get_db)):
